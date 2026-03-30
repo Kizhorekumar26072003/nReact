a1fdf7d54133abcb31230f931a729512c86f495d
Your Name <venk01atesh@gmal.com>
2026-03-30 20:02:50 +0530

    swiggy actual api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,17 +6,23 @@ import Shimmer from "./shimmer";
 
 const Body = () => {
   let [res, setRes] = useState([]);
+  let [searchText, setSearchText] = useState("");
+  let [searchArray, setSearchArray] = useState([]);
+  let [toggle, setToggle] = useState(false);
   useEffect(() => {
     fetchData();
   }, []);
   async function fetchData() {
-    const data = await fetch("https://namastedev.com/api/v1/listRestaurants");
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0279117&lng=80.26051369999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+    );
     const res = await data.json();
-    console.log(res);
 
     setRes(
-      res?.data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
-        ?.restaurants,
+      res?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants,
+    );
+    setSearchArray(
+      res?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants,
     );
   }
   return res.length === 0 ? (
@@ -24,32 +30,40 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="search">
+        <div>
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+            onKeyUp={() => {
+              let filteredArray = res.filter((r) =>
+                r.info.name.toLowerCase().includes(searchText.toLowerCase()),
+              );
+              setSearchArray(filteredArray);
+            }}
+          />
+          <button className="search-btn">Search</button>
+        </div>
+
         <button
           onClick={() => {
-            setRes(
+            setToggle(toggle === true ? false : toggle);
+            setSearchArray(
               res.filter((restaurant) => restaurant.info.avgRating >= 4.2),
             );
           }}
         >
-          Search
+          Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {res.map((restaurant) => (
+        {searchArray.map((restaurant) => (
           //should give key property to components especially components oon loop. otherwise react wwill re render all the same level elements.
           <RestaurantCard res={restaurant} key={restaurant.info.id} />
         ))}
-        {/* <RestaurantCard resName="" cuisine="" />
-        <RestaurantCard resName="" cuisine="" />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard /> */}
       </div>
     </div>
   );
--- src/components/RestaurnantCard.jsx
+++ src/components/RestaurnantCard.jsx
@@ -1,20 +1,22 @@
+import { CLOUDINARY_BASE_URL } from "../utils/constants";
+
 const styleCard = {
   backgroundColor: "#f0f0f0",
 };
 
 export const RestaurantCard = (props) => {
-  console.log("====================================");
-  console.log(props);
-  console.log("====================================");
   const { avgRating, cloudinaryImageId, cuisines, costForTwo, name } =
     props?.res.info;
-  console.log(cloudinaryImageId);
 
   return (
     <div className="res-card" style={styleCard}>
-      <img alt="logo" src={cloudinaryImageId} className="res-logo" />
+      <img
+        alt="logo"
+        src={`${CLOUDINARY_BASE_URL}/${cloudinaryImageId}`}
+        className="res-logo"
+      />
       <h3>{name}</h3>
-      <h4>{cuisines.map((c) => c)}</h4>
+      <h4>{cuisines.join(", ")}</h4>
 
       <h4>{avgRating}</h4>
       <h4>{costForTwo}</h4>
